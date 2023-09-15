d103af706d1ea9ca1c7a47bf927c91408bb79fb2
Aniket Pandit <115357320+aniketpandit07@users.noreply.github.com>
2023-09-15 16:06:49 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId =34546;
+let accountEmail = "anik@gmail.com";
+var accountPassword = "12345";
+accountCity = "Pune";
+let accountState;
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,13 @@
+// "use strict"; //treat all code as a newer version
+
+// alert(3+3)  we are using node js and not browser
+
+//  datatypes
+var name = "Aniket";  // strings datatype
+let age = 22; // int datatype
+let isLoggedIn = true; //boolean datatype
+//null is standalone value  and its Object type value
+//undefined is value is not assigned yet 
+//symbols
+
+console.log(typeof null);
