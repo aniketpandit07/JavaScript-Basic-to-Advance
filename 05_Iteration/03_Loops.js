f6f7b3e3efce9aeb5488e3b268ea19999c0dfc34
Aniket Pandit <aniketpandit548@gmail.com>
2023-11-18 19:28:06 +0530

    basic js completed

---
+++ 05_Iteration/03_Loops.js
@@ -0,0 +1,94 @@
+//for each example
+const coding =[2,3,4,6,8,9]
+
+const values = coding.forEach((item) => {
+    return item
+});
+//console.log(values);  //undefined   for each loop doesn't return anything
+
+
+// ++++++++ filter() ++++++++
+//filter method return the values based on condition
+const myNums =[33,676,99,43,24,6,8,9,65]
+
+let newNum =myNums.filter((num) => num>30)
+console.log(newNum);
+
+
+//when you use curly braces or open scope then return is MUST
+let num2 = myNums.filter((num) => {
+    return num>50
+})
+ console.log(num2);       //[ 676, 99, 65 ]
+
+
+
+ //above example using for each operarion
+//1 create an empty array
+const newNums = []
+
+//2apply for each method on previously defined array
+myNums.forEach((num)=>{
+    //3apply condition to filter it out
+    if(num>50){
+        //4 assign filtered value in an empty array
+        newNums.push(num)
+    }
+})
+//5 print it out
+console.log(newNums);          //[ 676, 99, 65 ]
+
+
+
+const books = [
+    {
+        title: 'Rich dad Poor dad', 
+        genre: 'Finance',
+        price: 299
+    },
+    {
+        title: 'Think like a Monk', 
+        genre: 'SelfHelp',
+        price: 179
+    },
+    {
+        title: 'Da vinci Code', 
+        genre: 'Sci-fi',
+        price: 499
+    },
+    {
+        title: 'The Alchemist', 
+        genre: 'SelfHelp',
+        price: 299
+    },
+    {
+        title: 'Wnings of Fire', 
+        genre: 'Biography',
+        price: 499
+    },
+    {
+        title: 'Psychology of Money', 
+        genre: 'Finance',
+        price: 250
+    },
+    {
+        title: 'Revolution 2020', 
+        genre: 'Fiction',
+        price: 150
+    }
+]
+
+//applying filter on books where it returns the finance books
+let userBooks = books.filter((bk)=> bk.genre=='Finance')
+console.log(userBooks);
+
+//return books finance and price is more than 100
+userBooks = books.filter((bk)=>bk.genre =='Finance' && bk.price>=100)
+
+console.log(userBooks);
+
+
+
+
+
+
---
+++ 05_Iteration/04_Loops.js
@@ -0,0 +1,53 @@
+const myNum = [1,2,3,4,5,5,6,7,8,9]
+
+//using map
+const add10 = myNum.map((item)=>item + 10)
+console.log(add10);
+
+//chaining in Map
+const newNums = myNum 
+            .map((num)=> num*10)    //[10,20,30,40,.....]
+            .map((num)=>num+1)      //[11,21.31,41,.....]
+console.log(newNums);
+
+//filter used in chaining map
+const newNums2 = myNum 
+            .map((num)=> num*10)    //[10,20,30,40,.....]
+            .map((num)=>num+1)      //[11,21.31,41,.....]
+            .filter((num)=>num>=40) //[41,51,61,.......]     
+console.log(newNums2);
+
+
+// +++++++ reduce() +++++++
+
+//in the begining value of accumelator is 0 then it take the value from previous operation
+const numArr = [1,2,4]
+
+const total = numArr.reduce(function (accumelator, currentValue){
+    //console.log(`acc: ${accumelator} and currrval: ${currentValue}`);
+    return accumelator + currentValue
+},0) 
+console.log(total);
+
+//reduce using arrow function
+const newTotal = numArr.reduce((acc,curval)=> acc + curval, 0)
+console.log(newTotal);
+
+//example
+const shopCart = [
+    {
+        item: 'js',
+        price: 1999
+    },
+    {
+        item: 'py',
+        price: 999
+    },
+    {
+        item: 'cpp',
+        price: 2999
+    } 
+]
+
+const cartTotal = shopCart.reduce((acc, currentval)=> acc+currentval.price,0)
+console.log(cartTotal);
